ab89523caceb5dda6cf009f4a06c7a8247272a30
satoken912 <seat.soil.74196901@gmail.com>
2018-05-20 01:38:06 +0900

    Modify article ID format, using list()

--- php/connectToMySQL.php
+++ php/connectToMySQL.php
@@ -5,7 +5,7 @@ function showActivities( $_connection, $_year ){
     $date_start = $_year ."-04-01";
     $date_end = ($_year+1) ."-03-31";
 
-    $q = "SELECT *, DATE_FORMAT(date, '%y%m%d') FROM activity WHERE date >= '$date_start' AND date <= '$date_end' ORDER BY date DESC";
+    $q = "SELECT *, DATE_FORMAT(date, '%y%m%d') as date_format FROM activity WHERE date >= '$date_start' AND date <= '$date_end' ORDER BY date DESC";
 
     $mode = "";
     if( isset($_GET["mode"]) ){
@@ -13,29 +13,30 @@ function showActivities( $_connection, $_year ){
     }
 
     foreach( $_connection->query( $q ) as $row ){
+        list($id, $date, $category, $title, $dates_str, $description, $thumbnail_url, $date_format) = $row;
 
-        if( $row[7] == 0 && $mode != "admin" ){
+        if( $date_format == 0 && $mode != "admin" ){
             continue;
         }
 
         $item  = "<div class='item'>";
 
-        $item .=    "<div class='item-margin' id='$row[8]'></div>";
+        $item .=    "<div class='item-margin' id='$id'></div>";
 
         $item .=    "<div class='item-heading'>";
-        $item .=        "<img class='symbol' src='https://satoken.nkmr.io/img/activity/$row[2].png'>";
+        $item .=        "<img class='symbol' src='https://satoken.nkmr.io/img/activity/$category.png'>";
         $item .=        "<div class='achievement-heading'>";
-        $item .=            $row[3];
-        $item .=            "<div class='achievement-heading-date'>$row[4]</div>";
+        $item .=            $title;
+        $item .=            "<div class='achievement-heading-date'>$dates_str</div>";
         $item .=        "</div>";
         $item .=    "</div>";
 
         $item .=    "<div class='item-desc'>";
-        $item .=        $row[5];
+        $item .=        $description;
         $item .=    "</div>";
 
         $item .=    "<div class='item-thumbnail'>";
-        $item .=        "<img src='$row[6]'>";
+        $item .=        "<img src='$thumbnail_url'>";
         $item .=    "</div>";
 
         $item .= "</div>";
